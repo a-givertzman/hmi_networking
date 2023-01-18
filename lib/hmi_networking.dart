282bda5b4729485a843e9c674ca1d068d14c104e
Minyewoo <const.trushov@gmail.com>
2023-01-18 14:50:36 +0300

    Added DoubleContainer

--- lib/hmi_networking.dart
+++ lib/hmi_networking.dart
@@ -7,5 +7,8 @@ export 'src/core/ds_line_socket.dart';
 export 'src/core/line_socket.dart';
 export 'src/core/stream_mearged.dart';
 export 'src/core/stream_mearged_or.dart';
+export 'src/core/ds_data_stream_extract.dart';
+export 'src/core/ds_data_point_extracted.dart';
+export 'src/core/double_container.dart';
 export 'src/protocols/custom_protocol_line.dart';
 export 'src/protocols/jsd_line.dart';
