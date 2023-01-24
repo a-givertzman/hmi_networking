f04744cbf3401e333c56a06c1c781c2d06b9c384
Minyewoo <const.trushov@gmail.com>
2023-01-24 14:09:50 +0300

    Added DsSend constructor test

---
+++ test/unit/core/ds_send/ds_send_constructor_test.dart
@@ -0,0 +1,55 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hmi_core/hmi_core.dart';
+import 'package:hmi_networking/hmi_networking.dart';
+import 'package:hmi_networking/src/core/ds_send.dart';
+
+import 'fake_ds_client.dart';
+
+void main() {
+  group('DsSend constructor', () {
+    test('asserts if unsupported type was provided', () {
+      expect(
+        () => DsSend<String>(
+          dsClient: FakeDsClient(),
+          pointPath: DsPointPath(path: '', name: ''),
+        ), 
+        throwsA(isA<AssertionError>()),
+        reason: 'DsSend constructor didn`t assert on unsupported type',
+      );
+      expect(
+        () => DsSend<DateTime>(
+          dsClient: FakeDsClient(),
+          pointPath: DsPointPath(path: '', name: ''),
+        ), 
+        throwsA(isA<AssertionError>()),
+        reason: 'DsSend constructor didnt`t assert on unsupported type',
+      );
+    });
+     test('completes normally if supported type was provided', () {
+      expect(
+        () => DsSend<bool>(
+          dsClient: FakeDsClient(),
+          pointPath: DsPointPath(path: '', name: ''),
+        ), 
+        returnsNormally,
+        reason: 'DsSend constructor asserted on supported type',
+      );
+      expect(
+        () => DsSend<int>(
+          dsClient: FakeDsClient(),
+          pointPath: DsPointPath(path: '', name: ''),
+        ), 
+        returnsNormally,
+        reason: 'DsSend constructor asserted on supported type',
+      );
+      expect(
+        () => DsSend<double>(
+          dsClient: FakeDsClient(),
+          pointPath: DsPointPath(path: '', name: ''),
+        ), 
+        returnsNormally,
+        reason: 'DsSend constructor asserted on supported type',
+      );
+    });
+  });
+}
---
+++ test/unit/core/ds_send/fake_ds_client.dart
@@ -0,0 +1,90 @@
+import 'package:hmi_core/src/core/result/result.dart';
+import 'package:hmi_core/src/core/entities/ds_data_point.dart';
+import 'package:hmi_core/src/core/entities/ds_command.dart';
+import 'package:hmi_networking/src/core/ds_client.dart';
+import 'package:hmi_networking/src/core/stream_mearged.dart';
+
+class FakeDsClient implements DsClient {
+  @override
+  bool isConnected() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Result<bool>> requestAll() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Result<bool>> requestNamed(List<String> names) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Result<bool> requestNamedEmulated(List<String> names) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Result<bool>> send(DsCommand dsCommand) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Result<bool> sendEmulated(DsCommand dsCommand) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<DsDataPoint<T>> stream<T>(String name) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<DsDataPoint<bool>> streamBool(String name, {bool inverse = false}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<DsDataPoint<bool>> streamBoolEmulated(String filterByValue, {int delay = 100}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<DsDataPoint<double>> streamEmulated(String filterByValue, {int delay = 100, double min = 0, double max = 100, int firstEventDelay = 0}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<DsDataPoint<int>> streamEmulatedInt(String filterByValue, {int delay = 100, double min = 0, double max = 100, int firstEventDelay = 0}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<DsDataPoint<int>> streamInt(String name, {int offset = 0}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<DsDataPoint> streamMerged(List<String> names) {
+    throw UnimplementedError();
+  }
+
+  @override
+  StreamMerged<DsDataPoint> streamMergedEmulated(List<String> names) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<DsDataPoint<double>> streamReal(String name, {double offset = 0.0}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<DsDataPoint<double>> streamRequestedEmulated(String filterByValue, {int delay = 500, double min = 0, double max = 100}) {
+    throw UnimplementedError();
+  }
+
+  @override
+  int get subscriptionsCount => throw UnimplementedError();
+}
