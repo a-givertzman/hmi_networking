63f69387f2bbf33b3048ebf3179f2953e9c7e86f
Minyewoo <const.trushov@gmail.com>
2024-03-05 15:22:39 +0300

    JdsDataPoint | Implemented DsDataPoint

--- lib/src/core/entities/jds_data_point.dart
+++ lib/src/core/entities/jds_data_point.dart
@@ -1,32 +1,108 @@
 import 'package:hmi_core/hmi_core.dart';
 /// 
 /// [DsDataPoint] with json transformation capabilities.
-class JdsDataPoint {
-  final DsDataPoint value;
+final class JdsDataPoint<T> implements DsDataPoint<T> {
+  final DsDataPoint<T> _point;
   /// 
   /// [DsDataPoint] with json transformation capabilities. 
-  const JdsDataPoint(this.value);
+  const JdsDataPoint(this._point);
   /// 
   /// Deserializes [DsDataPoint] from [json].
-  factory JdsDataPoint.fromJson(Map<String,dynamic> json) => JdsDataPoint(
-    DsDataPoint(
-      type: DsDataType.fromString(json['type']), 
-      name: DsPointName(json['name']), 
-      value: json['value'], 
-      status: DsStatus.fromValue(json['status']), 
-      history: json['history'],
-      alarm: json['alarm'],
-      timestamp: json['timestamp'],
-    ),
-  );
+  factory JdsDataPoint.fromMap(Map<String,dynamic> json) {
+    final type = DsDataType.fromString(json['type'] as String);
+    final name = DsPointName(json['name'] as String);
+    final value = '${json['value']}';
+    final status = DsStatus.fromValue(json['status'] as int);
+    final history = json['history'] as int? ?? 0;
+    final alarm = json['alarm'] as int? ?? 0;
+    final timestamp = json['timestamp'] as String;
+    return JdsDataPoint<T>(
+      switch(type) {
+        DsDataType.bool => DsDataPoint<bool>(
+          type: type, 
+          name: name, 
+          value: int.parse(value) > 0, 
+          history: history, 
+          alarm: alarm, 
+          status: status, 
+          timestamp: timestamp,
+        ) as DsDataPoint<T>,
+        DsDataType.integer || 
+        DsDataType.uInt    || 
+        DsDataType.dInt    || 
+        DsDataType.word    || 
+        DsDataType.lInt    => DsDataPoint<int>(
+          type: type, 
+          name: name, 
+          value: int.parse(value), 
+          history: history, 
+          alarm: alarm, 
+          status: status, 
+          timestamp: timestamp,
+        ) as DsDataPoint<T>,
+        DsDataType.real => DsDataPoint<double>(
+          type: type, 
+          name: name, 
+          value: double.parse(value), 
+          history: history, 
+          alarm: alarm, 
+          status: status, 
+          timestamp: timestamp,
+        ) as DsDataPoint<T>,
+        DsDataType.time || DsDataType.dateAndTime  => throw Failure.convertion(
+          message: 'Ошибка в методе $JdsDataPoint.fromJson(): тип $type не поддерживается',
+          stackTrace: StackTrace.current,
+        ),
+      },
+    );
+  }
   /// 
-  /// Serializes [DsDataPoint] to json.
-  Map<String, dynamic> toJson() => {
-      'type': value.type.value,
-      'name': value.name.toString(),
-      'value': value.value,
-      'status': value.status.value,
-      'history': value.history,
-      'alarm': value.alarm,
+  /// Converts [DsDataPoint] to [Map] eligible to further serialization.
+  Map<String, dynamic> toMap() => {
+      'type': _point.type.value,
+      'name': _point.name.toString(),
+      'value': switch(_point.type){
+        DsDataType.bool => _point.value as bool  ? 1 : 0,
+        DsDataType.integer || 
+        DsDataType.uInt    || 
+        DsDataType.dInt    || 
+        DsDataType.word    || 
+        DsDataType.lInt    ||
+        DsDataType.real    => _point.value.toString(),
+        DsDataType.time || DsDataType.dateAndTime  => throw Failure.convertion(
+          message: 'Ошибка в методе $JdsDataPoint.fromJson(): тип ${_point.type} не поддерживается',
+          stackTrace: StackTrace.current,
+        ),
+      },
+      'status': _point.status.value,
+      'history': _point.history,
+      'alarm': _point.alarm,
   };
+  //
+  @override
+  DsDataType get type => _point.type;
+  //
+  @override
+  DsPointName get name => _point.name;
+  //
+  @override
+  T get value => _point.value;
+  //
+  @override
+  DsStatus get status => _point.status;
+  //
+  @override
+  int get alarm => _point.alarm;
+  //
+  @override
+  int get history => _point.history;
+  //
+  @override
+  String get timestamp => _point.timestamp;
+  //
+  @override
+  String toJson() => _point.toJson();
+  //
+  @override
+  String toString() => _point.toString();
 }
