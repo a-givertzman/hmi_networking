47be35032c73939298b64f0a7988b98fe49bde85
Minyewoo <const.trushov@gmail.com>
2024-03-12 16:00:53 +0300

    UpdatePointsCacheFromJdsService | Added config request into apply function

--- lib/src/core/ds_client/cache/update_cache_from_point_configs.dart
+++ lib/src/core/ds_client/cache/update_cache_from_point_configs.dart
@@ -1,33 +1,42 @@
 import 'package:hmi_core/hmi_core.dart';
-import 'ds_client_cache.dart';
-import '../../jds_service/jds_point_config/jds_point_configs.dart';
+import 'package:hmi_core/hmi_core_result_new.dart';
+import 'package:hmi_networking/src/core/ds_client/cache/ds_client_cache.dart';
+import 'package:hmi_networking/src/protocols/jds_service/jds_service.dart';
 ///
-class UpdateCacheFromPointConfigs {
+class UpdatePointsCacheFromJdsService {
   final DsClientCache _cache;
-  final JdsPointConfigs _configs;
+  final JdsService _jdsService;
   ///
-  const UpdateCacheFromPointConfigs({
+  const UpdatePointsCacheFromJdsService({
     required DsClientCache cache, 
-    required JdsPointConfigs configs,
-  }) : _cache = cache, _configs = configs;
+    required JdsService jdsService,
+  }) : 
+    _cache = cache, 
+    _jdsService = jdsService;
   ///
-  Future<void> apply() async {
+  Future<ResultF<void>> apply() async {
     final cachePointNames = (await _cache.getAll())
-          .map((point) => point.name.toString()).toSet();
-    final configPointNames = _configs.names.toSet();
-    final newPointNames = configPointNames.where(
-      (pointName) => !cachePointNames.contains(pointName),
-    );
-    _cache.addMany(
-      newPointNames.map(
-        (pointName) => DsDataPoint(
-          type: DsDataType.integer,
-          name: DsPointName(pointName),
-          value: 0,
-          status: DsStatus.invalid,
-          timestamp: DsTimeStamp.now().toString(),
-        ),
-      ),
-    );
+      .map((point) => point.name.toString()).toSet();
+    switch(await _jdsService.points()) {
+      case Ok(value:final pointConfigs):
+        final configPointNames = pointConfigs.names.toSet();
+        final newPointNames = configPointNames.where(
+          (pointName) => !cachePointNames.contains(pointName),
+        );
+        await _cache.addMany(
+          newPointNames.map(
+            (pointName) => DsDataPoint(
+              type: DsDataType.integer,
+              name: DsPointName(pointName),
+              value: 0,
+              status: DsStatus.invalid,
+              timestamp: DsTimeStamp.now().toString(),
+            ),
+          ),
+        );
+        return const Ok(null);
+      case Err(:final error):
+        return Err(error);
+    }
   }
 }
