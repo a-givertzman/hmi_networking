1da2227b866a58396aac861e85cf6ca4a220ba9f
Minyewoo <const.trushov@gmail.com>
2024-03-21 19:05:22 +0300

    JdsServiceStartup | Added auth retries

--- lib/src/core/jds_service/jds_service_startup.dart
+++ lib/src/core/jds_service/jds_service_startup.dart
@@ -1,3 +1,4 @@
+import 'package:hmi_core/hmi_core_log.dart';
 import 'package:hmi_core/hmi_core_result_new.dart';
 import 'package:hmi_networking/src/core/ds_client/cache/ds_client_cache.dart';
 import 'package:hmi_networking/src/core/jds_service/update_cache_from_jds_service.dart';
@@ -6,8 +7,10 @@ import 'package:hmi_networking/src/core/jds_service/jds_service.dart';
 /// 
 /// [JdsService] startup sequence.
 class JdsServiceStartup {
+  static const _log = Log('JdsServiceStartup');
   final JdsService _service;
   final DsClientCache? _cache;
+  final Duration _authRetryDelay;
   ///
   /// [JdsService] startup sequence.
   /// 
@@ -15,18 +18,27 @@ class JdsServiceStartup {
   const JdsServiceStartup({
     required JdsService service,
     DsClientCache? cache,
+    Duration authRetryDelay = const Duration(milliseconds: 1000),
   }) : 
     _service = service,
-    _cache = cache;
+    _cache = cache,
+    _authRetryDelay = authRetryDelay;
   ///
   /// Pull points config from JDS service,
   /// save it to cache (if provided) 
   /// and subscribe for all of the points.
   Future<ResultF<void>> run() async {
-    // await _service.authenticate('');
+    _log.info('Authenting...');
+    while(await _service.authenticate('12345') is Err) {
+      _log.warning('Unable to authenticate.');
+      await Future.delayed(_authRetryDelay);
+      _log.warning('Retrying...');
+    }
+    _log.info('Pulling points config...');
     final configResult = await _service.points();
     switch(configResult) {
       case Ok(value:final config):
+        _log.info('Points config successfully pulled!');
         final cache = _cache;
         if(cache != null) {
           await UpdateCacheFromJdsService(
@@ -34,9 +46,10 @@ class JdsServiceStartup {
             jdsService: _service,
           ).apply();
         }
-        await _service.subscribe(config.names);
-        return const Ok(null);
+        _log.info('Subscribing to received points...');
+        return _service.subscribe(config.names);
       case Err(:final error):
+        _log.warning('Failed to pull points');
         return Err(error);
     }
   }
