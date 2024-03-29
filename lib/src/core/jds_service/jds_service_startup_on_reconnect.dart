12e71324df82b2c5e0a0fbabe832d4672136a299
Minyewoo <const.trushov@gmail.com>
2024-03-29 17:16:44 +0300

    JdsServiceStartupOnReconnect | Made startup cancellable

--- lib/src/core/jds_service/jds_service_startup_on_reconnect.dart
+++ lib/src/core/jds_service/jds_service_startup_on_reconnect.dart
@@ -5,21 +5,26 @@ import 'package:hmi_networking/src/core/jds_service/jds_service_startup.dart';
 /// 
 /// [JdsService] cache update sequence.
 class JdsServiceStartupOnReconnect {
-  final Stream<bool> _connectionStatuses;
+  final Stream<DsDataPoint<int>> _connectionStatuses;
   final JdsServiceStartup _startup;
   bool _isConnected;
+  StreamSubscription<bool>? _connectionSubscription;
+  final Completer _startupCompleter = Completer();
+  // final CancelableCompleter _startupCompleter = CancelableCompleter();
+  // Future? _startupFuture;
   ///
   /// [JdsService] cache update sequence.
-  /// 
-  /// [jdsService] - to pull config from.
-  /// 
-  /// [cache] - to save config to.
   JdsServiceStartupOnReconnect({
     required Stream<DsDataPoint<int>> connectionStatuses,
     required JdsServiceStartup startup,
     bool initialConnectionStatus = false,
   }) :
-    _connectionStatuses = connectionStatuses.map(
+    _connectionStatuses = connectionStatuses,
+    _startup = startup, 
+    _isConnected = initialConnectionStatus;
+  ///
+  void run() {
+    _connectionSubscription ??= _connectionStatuses.map(
       (point) => switch(point) {
         DsDataPoint<int>(
           value: final connectionStatus, 
@@ -28,16 +33,19 @@ class JdsServiceStartupOnReconnect {
         ) => connectionStatus == DsStatus.ok.value,
         _ => false
       },
-    ),
-    _startup = startup, 
-    _isConnected = initialConnectionStatus;
-  ///
-  StreamSubscription<bool> run() {
-    return _connectionStatuses.listen((isConnected) async {
+    ).listen((isConnected) async {
       if(_isConnected != isConnected) {
         _isConnected = isConnected;
-        await _startup.run();
+        if(!_startupCompleter.isCompleted) {
+          _startupCompleter.complete(null);
+        }
+        _startupCompleter.complete(_startup.run());
       }
     });
+  }
+
+  Future<void> cancel() async {
+    await _connectionSubscription?.cancel();
+    _startupCompleter.complete(null);
   }
 }
